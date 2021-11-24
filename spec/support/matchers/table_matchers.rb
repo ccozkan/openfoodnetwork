6690f9ceee09c111f2b16a1f007f4df32a79fedc
Çağrı Özkan <ccozkan@tutanota.com>
2021-11-24 13:33:50 +0400

    Fix rubocop issue Style/NegatedIfElseCondition

--- spec/support/matchers/table_matchers.rb
+++ spec/support/matchers/table_matchers.rb
@@ -29,10 +29,7 @@ RSpec::Matchers.define(:match_table) do |expected_table|
   match do |node|
     rows = node.all("tr").map { |r| r.all("th,td").map { |c| c.text.strip } }
 
-    if rows.count != expected_table.count
-      @failure_message = "found table with #{rows.count} rows, expected #{expected_table.count}"
-
-    else
+    if rows.count == expected_table.count
       rows.each_with_index do |row, i|
         expected_row = expected_table[i]
         if row.count != expected_row.count
@@ -49,6 +46,9 @@ RSpec::Matchers.define(:match_table) do |expected_table|
           break if @failure_message
         end
       end
+    else
+      @failure_message = "found table with #{rows.count} rows, expected #{expected_table.count}"
+
     end
 
     @failure_message.nil?
