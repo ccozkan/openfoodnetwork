39521b750833fccd9d377eb4179741d9248f7748
Çağrı Özkan <ccozkan@tutanota.com>
2021-11-24 12:45:36 +0400

    Fix rubocop issue Layout/FirstHashElementIndentation

--- spec/services/order_syncer_spec.rb
+++ spec/services/order_syncer_spec.rb
@@ -552,8 +552,8 @@ describe OrderSyncer do
             subscription.save # this is necessary to get an id on the subscription_line_items
             params = { subscription_line_items_attributes: [
 {
-              id: subscription.subscription_line_items.last.id, quantity: 2
-            }] }
+  id: subscription.subscription_line_items.last.id, quantity: 2
+}] }
             subscription.assign_attributes(params)
             expect(syncer.sync!).to be true
 
