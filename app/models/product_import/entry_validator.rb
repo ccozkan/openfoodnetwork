af84b5c64d7d89c48e4aadb813fb3f2ac57d7fa4
Çağrı Özkan <ccozkan@tutanota.com>
2021-11-24 12:49:43 +0400

    Fix rubocop issue Layout/ParameterAlignment

--- app/models/product_import/entry_validator.rb
+++ app/models/product_import/entry_validator.rb
@@ -11,7 +11,7 @@ module ProductImport
     # rubocop:disable Metrics/ParameterLists
     def initialize(
       current_user, import_time, spreadsheet_data, editable_enterprises,
-                   inventory_permissions, reset_counts, import_settings, all_entries
+      inventory_permissions, reset_counts, import_settings, all_entries
 )
       @current_user = current_user
       @import_time = import_time
