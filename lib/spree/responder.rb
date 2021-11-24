06f0dbfde3aa929b703136798e8edd239928a5f7
Çağrı Özkan <ccozkan@tutanota.com>
2021-11-24 13:33:41 +0400

    Fix rubocop issue Style/NegatedIf

--- lib/spree/responder.rb
+++ lib/spree/responder.rb
@@ -20,7 +20,7 @@ module Spree
     end
 
     def to_html
-      if !(on_success || on_failure)
+      unless (on_success || on_failure)
         super
         return
       end
@@ -29,7 +29,7 @@ module Spree
     end
 
     def to_format
-      if !(on_success || on_failure)
+      unless (on_success || on_failure)
         super
         return
       end
